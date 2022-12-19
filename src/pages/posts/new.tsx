2b3fb8d88ba867e60ce39e8943a5a1b18976797a
IvanRavic88 <ravic.ivan88@gmail.com>
2022-12-19 11:14:48 +0100

    Backbone code for app done 1

--- src/pages/posts/new.tsx
+++ src/pages/posts/new.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
-import { CreatePostInput } from "../../schema/post.schema";
+import type { CreatePostInput } from "../../schema/post.schema";
 import { trpc } from "../../utils/trpc";
 
 export default function CreatePostPage() {
